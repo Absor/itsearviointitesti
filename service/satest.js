0eb090d0b65293c31ff42bb453d34df996b69155
Heikki Haapala <hhaapala@gmail.com>
2014-08-08 09:44:08 +0300

    example data

--- service/satest.js
+++ service/satest.js
@@ -1,6 +1,14 @@
-angular.module('satest').factory('saTest',function() {
+angular.module('satest').factory('saTest',function($q) {
 
-	var saTest = {};
+	var saTest = {
+        all: function() {
+            var deferred = $q.defer();
+            deferred.resolve([{
+                name: "Test 1"
+            }]);
+            return deferred.promise;
+        }
+    };
 
 	return saTest;
 });
